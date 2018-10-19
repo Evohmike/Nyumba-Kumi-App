5f4dae6ec792a041ed618caf3370eabda54c4a18
Evohmike <evohmik@gmail.com>
2018-10-19 10:41:22 +0300

    created NeighbourHood class

--- app/models.py
+++ app/models.py
@@ -5,10 +5,30 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+
+  user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name="profile")
+  profile_pic = models.ImageField(upload_to='picture/', null=True, blank=True, default=0)
+  bio = models.TextField(max_length=200, null=True, blank=True, default="bio")
+  project = models.ForeignKey(Project, null=True)
+  email= models.TextField(max_length=200, null=True, blank=True, default=0)
+  neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,related_name="neighbourhood",null=True,blank=True)
+
+  def create_user_profile(sender, instance, created, **kwargs):
+      if created:
+          Profile.objects.create(user=instance)
+
+  post_save.connect(create_user_profile, sender=User)
+
+  def save_profile(self):
+      self.save()
+
+  def delete_profile(self):
+      self.delete()
+
+  @classmethod
+  def search_users(cls, search_term):
+      profiles = cls.objects.filter(user__username__icontains=search_term)
+      return profiles
 
 
 @receiver(post_save, sender=User)
@@ -20,5 +40,11 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+
+
+class NeighbourHood(models.Model):
+
 
 
