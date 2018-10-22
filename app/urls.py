84564a4a6e6ba7f6664b7e9e8fc03faaa2ebf071
Evohmike <evohmik@gmail.com>
2018-10-22 09:15:51 +0300

    added url for all-hood

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.index,name = 'home'),
+    url('^$',views.home,name = 'home'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+
+    url(r'^hoods', views.all_hoods, name='hoods'),
+
 ]
 
 if settings.DEBUG:
