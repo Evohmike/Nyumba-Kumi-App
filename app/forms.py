4bab105d3afbe9f32418ecde8b54b3a71eda39d9
Evohmike <evohmik@gmail.com>
2018-10-22 12:36:37 +0300

    added createhoodform

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from.models import Neighbourhood,Post,Profile, Business,Comments
+
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class CreateHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        fields = ['name','loc','occupants']
