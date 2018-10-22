b470484f9e2501ddaee2b219acc3c968ba07ee48
Evohmike <evohmik@gmail.com>
2018-10-22 12:38:55 +0300

    added redirect

--- hood/settings.py
+++ hood/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+LOGIN_REDIRECT_URL='home'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
